4c9c2d47964425d807261bb00c8b32faf04c6001
melek <melek_ertug@hotmail.com>
2017-08-05 00:04:14 +0300

    Bir dizi veri girilerek yapılmış isim tahmin uygulamasıdır. C# dili ile geliştirildi. WindowsForms uygulamasıdır.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace _20170412_giyotin
+{
+    partial class FormGiyotin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.tmrYaz = new System.Windows.Forms.Timer(this.components);
+            this.tmrSil = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(164, 12);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // tmrYaz
+            // 
+            this.tmrYaz.Interval = 500;
+            this.tmrYaz.Tick += new System.EventHandler(this.tmrSure_Tick);
+            // 
+            // tmrSil
+            // 
+            this.tmrSil.Interval = 500;
+            this.tmrSil.Tick += new System.EventHandler(this.tmrSil_Tick);
+            // 
+            // FormGiyotin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 443);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "FormGiyotin";
+            this.Text = " Giyotin";
+            this.Load += new System.EventHandler(this.FormGiyotin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Timer tmrYaz;
+        private System.Windows.Forms.Timer tmrSil;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace _20170412_giyotin
+{
+    public partial class FormGiyotin : Form
+    {
+        public FormGiyotin()
+        {
+            InitializeComponent();
+        }
+        int x, y, z, t, u;
+        string[] kisiler = new string[] { "Melek", "Seda", "Uğur", "Muhammed", "Kübra", "Özdecan", "Simon", "Yücel", "Ceren", "Eray", "Onur", "Esra", "Ahmet", "Dilan", "Ebubekir" };
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            tmrYaz.Enabled = true;
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = @"C:\Users\CASPER\Documents\Visual Studio 2012\Projects\20170412_giyotin\20170412_giyotin\psycho_theme (online-audio-converter.com).wav";
+            
+            player.Play();
+        }
+
+
+        private void tmrSure_Tick(object sender, EventArgs e)
+        {
+            x = rdm.Next(0, 400);
+            y = rdm.Next(0, 400);
+            z = rdm.Next(0, 255);
+            t = rdm.Next(0, 255);
+            u = rdm.Next(0, 255);
+
+            label[i] = new Label();
+            label[i].Text = kisiler[rdm.Next(0, 14)].ToString();
+            label[i].ForeColor = Color.FromArgb(z, t, u);
+            label[i].Location = new Point(x, y);
+            label[i].Name = "lbl" + i;
+            label[i].AutoSize = true;
+            labelisimler.Add(label[i].Name);
+            this.Controls.Add(label[i]);
+            if (i >= 29)
+            {
+                tmrYaz.Enabled = false;
+                tmrSil.Enabled = true;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        Label[] label = new Label[30];
+        int i = 0;
+        Random rdm = new Random();
+        List<string> labelisimler = new List<string>();
+        private void FormGiyotin_Load(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = @"C:\Users\CASPER\Documents\Visual Studio 2012\Projects\20170412_giyotin\20170412_giyotin\psycho_theme (online-audio-converter.com).wav";
+            
+        }
+
+        private void tmrSil_Tick(object sender, EventArgs e)
+        {
+            if (labelisimler.Count > 1)
+            {
+                int sayi = rdm.Next(0, labelisimler.Count);
+                Label lbl = (Label)this.Controls.Find(labelisimler[sayi], false)[0];
+                this.Controls.Remove(lbl);
+                labelisimler.RemoveAt(sayi);
+            }
+            else
+            {
+                tmrSil.Enabled = false;
+                Label lbl = (Label)this.Controls.Find(labelisimler[0], false)[0];
+                lbl.Font = new Font(lbl.Font.FontFamily, 20);
+                lbl.Location = new Point(178, 164);
+                //sonuncusu kaldığında yapılacak işlemon
+
+
+            }
+        }
+    }
+}
